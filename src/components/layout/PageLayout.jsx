2d595f973873fe7e18f646a8abd6f89292c935bc
rameez <rameezwang@gmail.com>
2026-03-20 01:19:58 +0530

    feat: Introduce a new `PageLayout` component for consistent structure and establish global styling with custom TailwindCSS themes and utilities.

--- src/components/layout/PageLayout.jsx
+++ src/components/layout/PageLayout.jsx
@@ -18,7 +18,7 @@ const PageLayout = ({ children }) => {
     ].includes(location.pathname);
 
     return (
-        <div className="flex flex-col min-h-screen">
+        <div className="flex flex-col min-h-screen w-full max-w-[100vw] overflow-x-hidden relative">
             {!isDivisionPage && <Navbar />}
             <main className={`flex-grow ${!isDivisionPage ? 'pt-[72px]' : ''}`}>
                 {children}
